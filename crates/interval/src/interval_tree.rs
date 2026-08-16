e664abd571ed850e04324e45bd40ebd88a15d0c7
ilijavlahovic24-bit <ilija.vlahovic24@gmail.com>
2026-08-16 10:09:54 +0200

    added interval tree

--- crates/interval/src/interval_tree.rs
+++ crates/interval/src/interval_tree.rs
@@ -1,16 +1,42 @@
 use chrono::NaiveDateTime;
+
 struct Interval{
     start_time:NaiveDateTime,
     end_time:NaiveDateTime,
 }
-struct IntervalTreeNode {
-    i:Interval,
-    max:u64,
-    left: Option<Box<IntervalTreeNode>>,
-    right: Option<Box<IntervalTreeNode>>,
+impl Interval{
+    fn new(start_time:NaiveDateTime, end_time:NaiveDateTime) -> Self {
+        Interval{start_time, end_time}
+    }
 }
 
+struct IntervalTreeNode {
+    i:Interval,
+    max:NaiveDateTime,
+    left: Option<Box<IntervalTreeNode>>,
+    right: Option<Box<IntervalTreeNode>>,
+
+}
+
+impl IntervalTreeNode {
+    fn new(i:Interval,max:u64) -> Self {
+        IntervalTreeNode{i,max,left:None,right:None}
+    }
+    fn add_left(&mut self, left:IntervalTreeNode){
+        self.left = Some(Box::new(left));
+    }
+    fn add_right(&mut self,right:IntervalTreeNode){
+        self.right = Some(Box::new(right));
+    }
+}
 struct IntervalTree {
     root:IntervalTreeNode,
-    
+}
+impl IntervalTree {
+    fn new(root:IntervalTreeNode) -> Self {
+        IntervalTree{root}
+    }
+    fn insert(&mut self, interval:Interval){
+        todo!()
+    }
 }
