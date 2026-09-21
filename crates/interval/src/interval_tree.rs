547ff4233d0adc266070c93e7e1f11ba3c18f124
ilijavlahovic24-bit <ilija.vlahovic24@gmail.com>
2026-09-21 19:20:28 +0200

    interval properties

--- crates/interval/src/interval_tree.rs
+++ crates/interval/src/interval_tree.rs
@@ -1,12 +1,24 @@
 use chrono::NaiveDateTime;
+use serde_json::Value;
+use std::collections::HashMap;
 
+#[derive(Clone,Debug,PartialEq)]
 struct Interval{
     start_time:NaiveDateTime,
     end_time:NaiveDateTime,
+    pub properties:HashMap<String,Value>,
 }
 impl Interval{
     fn new(start_time:NaiveDateTime, end_time:NaiveDateTime) -> Self {
-        Interval{start_time, end_time}
+        Interval{start_time, end_time,properties:HashMap::new()}
+    }
+    pub fn with_properties(mut self, properties:HashMap<String,Value>) -> Self {
+        self.properties = properties;
+        self
+    }
+    #[inline]
+    pub fn overlaps(&self, t1:NaiveDateTime, t2:NaiveDateTime) -> bool {
+        self.start_time <= t2 && self.end_time >= t1
     }
 }
 
